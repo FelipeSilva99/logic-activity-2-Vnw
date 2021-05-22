c0f59fea45b2d94fbaf22f48a8b11852da6a9c92
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-05-22 19:41:51 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+let idade = 22
+
+if(idade >= 18 ){
+    alert('Já pode ser preso')
+}
+
+
+
+// 2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+let human = prompt('Você é humano ?')
+
+if(idade => 18 && human == true){
+    console.log('Parabéns você é humano')
+}
+
+// 3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+const niver = 'Abril'
+if(niver == 'Maio' || niver == 'Abril'){
+    console.log('🎈🎉🎈 Parabéns 🎈🎉🎈')
+}else{
+    console.log('Infelizmente ainda não é sua vez.')
+}
+
+
+// *** Exercícios bônus
+// 4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+// 5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
